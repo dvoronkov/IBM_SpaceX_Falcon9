18d223bcf977dbf402ec3a98f5c9a0ba7dff41d4
Daria Voronkov <dmerkulenko@icloud.com>
2024-06-11 15:43:44 -0400

    Update 7-spacex_dash_app.py

--- Jupyter_Notebooks/7-spacex_dash_app.py
+++ Jupyter_Notebooks/7-spacex_dash_app.py
@@ -1,4 +1,5 @@
 # Import required libraries
+
 import pandas as pd
 import dash
 import dash_html_components as html
@@ -22,7 +23,7 @@ app = dash.Dash(__name__)
 app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
-                                # TASK 1: Add a dropdown list to enable Launch Site selection
+                                # Part 1: Add a dropdown list to enable Launch Site selection
                                 # The default select value is for ALL sites
                                 # dcc.Dropdown(id='site-dropdown',...)
                                 dcc.Dropdown(
@@ -33,14 +34,14 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                     searchable=True
                                 ),
 
-                                # TASK 2: Add a pie chart to show the total successful launches count for all sites
+                                # Part 2: Add a pie chart to show the total successful launches count for all sites
                                 # If a specific launch site was selected, show the Success vs. Failed counts for the site
                                 html.Div(dcc.Graph(id='success-pie-chart')),
                                 html.Br(),
 
                                 html.P("Payload range (Kg):"),
 
-                                # TASK 3: Add a slider to select payload range
+                                # Part 3: Add a slider to select payload range
                                 dcc.RangeSlider(id='payload-slider',
                                                 min=0, max=10000, step=1000,
                                                 marks={i: f'{i}' for i in range(0, 10001, 1000)},
@@ -48,11 +49,11 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 ),
                                 html.Br(),
 
-                                # TASK 4: Add a scatter chart to show the correlation between payload and launch success
+                                # Part 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
                                 ])
 
-# TASK 2: Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+# Part 2: Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
 @app.callback(
     Output(component_id='success-pie-chart', component_property='figure'),
     Input(component_id='site-dropdown', component_property='value')
@@ -68,7 +69,7 @@ def update_pie_chart(selected_site):
                      title=f'Total Successful Launches for site {selected_site}')
     return fig
 
-# TASK 4: Add a callback function to render the success-payload-scatter-chart scatter plot
+# Part 4: Add a callback function to render the success-payload-scatter-chart scatter plot
 @app.callback(
     Output(component_id='success-payload-scatter-chart', component_property='figure'),
     [Input(component_id='site-dropdown', component_property='value'), 
